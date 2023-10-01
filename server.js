cd0210ce6430b108737b5b45c9bc6340f50b8284
1F10230250 <s1f102302506@iniad.org>
2023-10-01 15:01:06 +0900

    練習

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("nodejs")
+const express =require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/",(req,res)=>{
+    console.log("hello express")
+    res.send("<h1>こんにちは</h1>");
+})
+
+app.listen(PORT,()=>console.log("サーバーが起動しました"))
