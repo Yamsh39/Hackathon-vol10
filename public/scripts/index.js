ea2ef4497a6e36d6b9c30d099acac199b1a30f1a
Yamsh39 <s1f102300043@iniad.org>
2023-10-06 23:34:08 +0900

    prettierの導入

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,2 +1,2 @@
 const toFormButton = document.getElementById("toFormButton");
-console.log("hello")
+console.log("hello");
--- server.js
+++ server.js
@@ -4,23 +4,26 @@ const contentsRouter = require("./routes/contents");
 
 const PORT = 8000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set("view engine", "ejs"); 
-app.use(express.static(__dirname+"/public"));
+app.set("view engine", "ejs");
 // app.use(mylogger);
 
-app.get("/", (req, res) =>{
-    // console.log("helloExpress");
-    // res.send("<h1>こんにちは</h1>");
-    // res.sendStatus(400);
-    // res.status(500).send("エラー");
-    // res.status(500).json({msg: "エラーです"});
-    res.render("index", {text: "NodejsとExpress"});
+app.get("/", (req, res) => {
+	// console.log("helloExpress");
+	// res.send("<h1>こんにちは</h1>");
+	// res.sendStatus(400);
+	// res.status(500).send("エラー");
+	// res.status(500).json({msg: "エラーです"});
+	res.render("index", { text: "NodejsとExpress" });
 });
 
-app.get("/form", (req, res) =>{
-    res.render("form");
+app.get("/form", (req, res) => {
+	res.render("form");
+});
+
+app.post("/form", (req, res) => {
+	res.redirect("/");
 });
 
 //ルーティング設計
@@ -29,12 +32,10 @@ app.use("/contents", contentsRouter);
 // app.use("/customer", customreRouter);
 // app.use("/product", productRouter);
 
-
-
 //ミドルウェア
 function mylogger(req, res, next) {
-    console.log(req.originalUrl);
-    next();
-};
+	console.log(req.originalUrl);
+	next();
+}
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
